d4322e8949b0f41f9ef89f8217926fb22074702b
shohjahon1code <suyunovshohjahon08@gmail.com>
2022-12-25 21:46:53 +0500

    finished

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,9 +1,9 @@
 const swiper = new Swiper(".swiper", {
   direction: "horizontal",
   loop: true,
-  autoplay: {
-    delay: 2000,
-  },
+  // autoplay: {
+  //   delay: 2000,
+  // },
 
 
   pagination: {
@@ -13,5 +13,8 @@ const swiper = new Swiper(".swiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
   },
 });
