0e6c451abecb71432fc40baa565f3d77a4b1f8c4
Hacker00002 <abdulbositsharipovv@gmail.com>
2022-12-25 17:46:52 +0500

    finish section cars

--- scripts/renderCars.js
+++ scripts/renderCars.js
@@ -1,7 +1,39 @@
 const elCards = document.querySelector(".rent-cards__all-items");
+const elBtn1 = document.querySelector(".rent-cards__buttons");
+
+function renderTab(items) {
+  let arrCards = [];
+
+  items.forEach((render) => {
+    render.category.forEach((categoryElem) => {
+      if (!arrCards.includes(categoryElem)) {
+        arrCards.push(categoryElem);
+      }
+    });
+  });
+  arrCards.forEach((render) => {
+    const buttonA = document.createElement("a");
+    buttonA.classList.add("rent-cards__CompactButton");
+    buttonA.style.cursor = "pointer";
+    elBtn1.appendChild(buttonA);
+    buttonA.textContent = render;
+  });
+
+  elBtn1.addEventListener("click", (evt) => {
+    const fileredArray = arr.filter((item) =>
+      item.category.includes(evt.target.textContent)
+    );
+    if (fileredArray.length > 0) {
+      render(fileredArray);
+    }
+  });
+}
+
+renderTab(arr);
 
 function render(items) {
-  items.forEach((element) => {
+  elCards.innerHTML = "";
+  items.slice(0, 6).forEach((element) => {
     const newDiv = document.createElement("div");
 
     newDiv.innerHTML = `
